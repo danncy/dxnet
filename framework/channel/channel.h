d23d69ce62c38c08cbb86dcb36a0d650b364f09d
danncy <hewenliang2008@163.com>
2018-04-22 17:53:26 +0800

    add contructor for channel option.

--- framework/channel/channel.h
+++ framework/channel/channel.h
@@ -1,6 +1,9 @@
 #ifndef FRAMEWORK_CHANNEL_H_
 #define FRAMEWORK_CHANNEL_H_
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
 namespace framework {
 
 struct Channel {
@@ -13,9 +16,15 @@ struct Channel {
   };
 
   struct Option {
-    Channel::Type type;
-    int sock_type;
-    int sock_proto;
+    Channel::Type type_;
+    int sock_type_  = SOCK_DGRAM;
+    int sock_proto_ = 0;
+
+    Option(Channel::Type type) : type_(type) {}
+    Option(Channel::Type type, int stype, int sproto)
+      : type_(type),
+        sock_type_(stype),
+        sock_proto_(sproto) {}
   };
 
   static Channel* Create(const Option& option);
