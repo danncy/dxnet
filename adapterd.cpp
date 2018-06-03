bc731a13afe382d52d60ae87d4723bd3b5075442
danncy <hewenliang2008@163.com>
2018-06-03 12:06:17 +0800

    add tcp:8000 listening in adapterd.

--- adapterd.cpp
+++ adapterd.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "framework/channel/channel_ipv4.h"
+#include "framework/main_loop/main_loop.h"
 
 int main(int argc, char** argv) {
   std::cout << "adapter init!" << std::endl;
+
+  framework::Channel::Option opt(framework::Channel::Type::SOCK_INET);
+  opt.sock_type = SOCK_STREAM;
+  opt.sock_port = 8000;
+  framework::ChannelIPv4 chn(opt);
+
+  framework::MainLoop mainloop;
+  mainloop.Run();
 
   return 0;
 }
