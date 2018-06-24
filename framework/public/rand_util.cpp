ea71a19199492f4aa2030be247d872afb2672283
danncy <hewenliang2008@163.com>
2018-06-24 09:05:40 +0800

    refact rand num function.

--- framework/public/rand_util.cpp
+++ framework/public/rand_util.cpp
@@ -13,15 +13,19 @@ namespace SeqNum {
 namespace framework {
 
 std::string GenerateUniqueRandomProcId() {
+  Format fmt("%1.%2.%3");
+  fmt % getpid() % SeqNum::GetNext() % GenerateRandomNumber();
+
+  return fmt.Buffer();
+}
+
+int GenerateRandomNumber() {
   std::random_device rdev;
   std::default_random_engine e(rdev());
   std::uniform_int_distribution<int> uniform_dist(1,
     std::numeric_limits<int>::max());
-  int rv = uniform_dist(e);
 
-  Format fmt("%1.%2.%3");
-  fmt % getpid() % SeqNum::GetNext() % rv;
-  return fmt.Buffer();
+  return uniform_dist(e);
 }
 
 }
--- framework/public/rand_util.h
+++ framework/public/rand_util.h
@@ -8,6 +8,7 @@
 namespace framework {
 
 std::string GenerateUniqueRandomProcId();
+int GenerateRandomNumber();
 
 } // namespace framework
 #endif
