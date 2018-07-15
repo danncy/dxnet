bf401215b0a46e1bde63a3b23110939790d79c0d
danncy <hewenliang2008@163.com>
2018-07-15 16:37:59 +0800

    replace log code.

--- framework/thread/thread.cpp
+++ framework/thread/thread.cpp
@@ -20,13 +20,12 @@ void Thread::Mainloop() {
 }
 
 bool Thread::Start() {
-  LOG(INFO) << _F("%1 thread name: %2") % __func__ % Name();
+  Log::info("%1 thread name: %2", __func__, Name());
   Options option;
   return StartWithOptions(option);
 }
 
 void* ThreadMain(void* arguments) {
-  LOG(INFO) << __func__;
   if (!arguments)
     return nullptr;
 
@@ -37,7 +36,6 @@ void* ThreadMain(void* arguments) {
 }
 
 bool Thread::StartWithOptions(const Options& option) {
-  LOG(INFO) << "StartWithOptions\n";
   pthread_attr_t attr;
   pthread_attr_init(&attr);
   bool success = true;
@@ -48,22 +46,18 @@ bool Thread::StartWithOptions(const Options& option) {
     pthread_attr_setstacksize(&attr, option.stack_size);
 
   int ret = pthread_create(&thread_id_, &attr, ThreadMain, this);
-  LOG(INFO) << ret;
   if (ret != 0) {
-    //log pthread_create failed.
-    LOG(ERROR) << ret;
+    Log::error("fail to create thread : %1", ret);
     pthread_attr_destroy(&attr);
     return false;
   }
 #ifdef _GNU_SOURCE
   ret = pthread_setname_np(thread_id_, name_.substr(0,15).c_str());
+  char thread_name[16] = {0};
+  pthread_getname_np(thread_id_, thread_name, 16);
   if (ret != 0) {
-    char thread_name[16] = {0};
-    pthread_getname_np(thread_id_, thread_name, 16);
     Log::error("%1,%2,%3,%4", ret, name_, std::strerror(ret), thread_name);
   } else {
-    char thread_name[16] = {0};
-    pthread_getname_np(thread_id_, thread_name, 16);
     Log::info("%1,%2,%3,%4", ret, name_, std::strerror(ret), thread_name);
   }
 #endif
